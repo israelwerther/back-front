21fc3b97ee0e9bc3ac60dcfc8c03fed3e2d271ed
israelwerther <israelwerther48@outlook.com>
2023-06-20 21:01:06 -0300

    remove old template

--- frontend/src/app/template/header/header.component.ts
+++ frontend/src/app/template/header/header.component.ts
@@ -7,13 +7,4 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent {
 
-  @Output() sideNavToggled = new EventEmitter<boolean>();
-
-  menuStatus: boolean = false;
-
-  SideNavToggle() {
-    this.menuStatus = !this.menuStatus;
-    this.sideNavToggled.emit(this.menuStatus)
-  }
-
 }
